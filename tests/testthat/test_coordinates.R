06195aea28f7f78c268bf8b5c95d6fcdc75943d5
Sebastian Gibb <mail@sebastiangibb.de>
2015-05-06 16:47:58 +0200

    add adjust argument

--- tests/testthat/test_coordinates.R
+++ tests/testthat/test_coordinates.R
@@ -3,9 +3,11 @@ context("coordinates")
 
 test_that("coordinates", {
   p <- list(createMassPeaks(mass=1:5, intensity=1:5,
-                            metaData=list(imaging=list(pos=c(1, 1)))),
+                            metaData=list(imaging=list(pos=c(5, 2)))),
             createMassPeaks(mass=1:5, intensity=5:1,
-                            metaData=list(imaging=list(pos=c(2, 1)))))
-  m <- matrix(c(1, 2, 1, 1), nrow=2, dimnames=list(c(), c("x", "y")))
-  expect_equal(coordinates(p), m)
+                            metaData=list(imaging=list(pos=c(6, 2)))))
+  m <- matrix(c(5, 6, 2, 2), nrow=2, dimnames=list(c(), c("x", "y")))
+  madjust <- matrix(c(1, 2, 1, 1), nrow=2, dimnames=list(c(), c("x", "y")))
+  expect_equal(coordinates(p, adjust=FALSE), m)
+  expect_equal(coordinates(p), madjust)
 })
